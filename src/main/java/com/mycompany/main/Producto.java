60f3afe31cd0b46f1d7ff807f6533209c66bb2d3
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-09-30 09:02:10 -0500

    se agrego el atributo stock

--- src/main/java/com/mycompany/main/Producto.java
+++ src/main/java/com/mycompany/main/Producto.java
@@ -4,10 +4,12 @@ public abstract class Producto {
 
     private String nombre;
     private Double precio;
+    int stock;
 
-    public Producto(String nombre, Double precio) {
+    public Producto(String nombre, Double precio, int stock) {
         this.nombre = nombre;
         this.precio = precio;
+        this.stock = stock;
     }
 
     public String getNombre() {
@@ -25,6 +27,17 @@ public abstract class Producto {
     public void setPrecio(Double precio) {
         this.precio = precio;
     }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+
+    
 
     protected abstract String getDescription();
 
