f9c014b25eac7365ce6182a79fbc842db3b95132
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-09-30 08:02:07 -0500

    creacion de los menu y el ciclo

--- src/main/java/com/mycompany/main/Tienda.java
+++ src/main/java/com/mycompany/main/Tienda.java
@@ -1,6 +1,36 @@
 package com.mycompany.main;
 
+import javax.swing.JOptionPane;
+
 public class Tienda {
-    
+
+    public void iniciar() {
+        int opcion;
+        do {
+
+            String menu = """
+                    ===MENU PRINCIPAL===
+                    1. Gestionar Alimentos
+                    2. Gestionar Electrodomésticos
+                    3. Salir
+                    """;
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(menu));
+            
+            switch (opcion) {
+                case 1:
+                    
+                    
+                    
+                case 2:
+                    
+                    
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null,"opcion invalida");
+            }
+
+        } while (opcion != 3);
+
+    }
 
 }
