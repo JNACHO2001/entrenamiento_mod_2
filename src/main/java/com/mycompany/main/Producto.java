14c30b6cf99ef3506f62130165f7eec67fa7e38f
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-09-30 07:42:07 -0500

    creacion del archivo producto

---
+++ src/main/java/com/mycompany/main/Producto.java
@@ -0,0 +1,31 @@
+package com.mycompany.main;
+
+public abstract class Producto {
+
+    private String nombre;
+    private Double precio;
+
+    public Producto(String nombre, Double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    protected abstract String getDescription();
+
+}
